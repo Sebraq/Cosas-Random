1f92472702a63bab51227ecd8ece83f55f23eaa5
Sebraq <65198928+Sebraq@users.noreply.github.com>
2023-01-19 11:34:18 -0600

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,28 @@
+# #Ejercicio1
+# print("----------Ejercicio1------------")
+# print('Hello, world!')
+
+# #Ejercicio 2 :Programa que almacene una cadena.
+# print("----------Ejercicio2------------")
+# mensaje="Hola mundo"
+# print(mensaje)
+
+# #Ejercicio 3:Escribir un programa que muestre el nombre del usuario.
+# print("----------Ejercicio3------------")
+# nombre=input("Ingrese su nombre :")
+# print("Hola,bienvenido "+nombre)
+
+#Ejercicio 4 :Programa que muestra una operacion aritmetica
+#(3+2/2*5)^2 Para elevar usar **
+# print("----------Ejercicio4------------")
+# print(((3+2)/(2*5))**2)
+
+#Ejercicio 5:Numeros de horas trabajadas y el coste por hora.
+print("----------Ejercicio5------------")
+
+nombre=input("Cual es su nombre? :")
+horas=float(input("Ingrese sus horas de trabajo: "))
+costo=float(input("Introduce lo que ganas por hora"))
+pago=horas*costo
+print(nombre," tu paga es de : ",pago,"Quetzales")
+
